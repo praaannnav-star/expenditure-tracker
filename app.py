25bec822a567d6d19f85935cf9759e8e50dacc7d
Pranav Kuamr <praaannnav@gmail.com>
2026-07-25 01:09:32 +0530

    Configure application to use SQLite database always

--- app.py
+++ app.py
@@ -9,12 +9,7 @@ from datetime import datetime
 app = Flask(__name__)
 
 # DB CONFIG
-db_url = os.getenv('DATABASE_URL', 'sqlite:///sqlite.db')
-
-if db_url.startswith("postgres://"):
-    db_url = db_url.replace("postgres://", "postgresql://", 1)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
 app.secret_key = os.getenv("SECRET_KEY", "dev")
 
 db = SQLAlchemy(app)
